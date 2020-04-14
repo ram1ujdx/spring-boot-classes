894920d0877e4ae680ff4cdf2e2288e463825828
ramanujdas <ramanujds9@gmail.com>
2020-04-14 18:46:57 +0530

    Added Model

--- spring-boot-demo/src/main/java/com/demo/springboot/controller/UserController.java
+++ spring-boot-demo/src/main/java/com/demo/springboot/controller/UserController.java
@@ -2,12 +2,19 @@ package com.demo.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 	
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "view.jsp";
+		return "view";
 	}
+	
+	//views/view.jsp
+	
+	
 }
